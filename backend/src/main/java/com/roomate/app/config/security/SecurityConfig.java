d757802e3c10f08bda56a12c4b61bb0f1f0e2793
Jagmans <jagmansidhu1@icloud.com>
2026-04-13 20:38:20 -0700

    Fixed issue with creating utilities and chores TODO: Issue with getting the chore and utilities for specific rooms

--- backend/src/main/java/com/roomate/app/config/security/SecurityConfig.java
+++ backend/src/main/java/com/roomate/app/config/security/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // @Profile("!test")
 public class SecurityConfig {
 
-    @Value("${app.cors.allowed-origins:http://localhost:3000,http://127.0.0.1:3000}")
+    @Value("${app.cors.allowed-origins:http://localhost:3000,http://127.0.0.1:8085}")
     private String allowedOrigins;
 
     @Bean
